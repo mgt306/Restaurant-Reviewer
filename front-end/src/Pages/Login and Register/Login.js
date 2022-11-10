955d2a38b1d01a7694d50b736c70d35a57a065bf
Derek <swh376@nyu.edu>
2022-11-10 01:37:55 -0500

    added google login verification, needs styling

--- front-end/src/Pages/Login and Register/Login.js
+++ front-end/src/Pages/Login and Register/Login.js
@@ -1,20 +1,60 @@
-import React, { useState } from "react";
+import React, { useState , useEffect} from "react";
 import { Link } from "react-router-dom";
 import "../../Styles/Login.css";
+import jwt_decode from "jwt-decode";
 
 
 export default function Login() {
     
-    const [loading, setLoading] = useState(false);
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        setLoading(true);
+    // const [loading, setLoading] = useState(false);
+    // const handleSubmit = (e) => {
+    //     e.preventDefault();
+    //     setLoading(true);
+    // }
+    const [user, setUser] = useState({});
+
+    function handleCallbackResponse(response) {
+      console.log("Encoded JWT ID Token: " + response.credential);
+      var userObject = jwt_decode(response.credential);
+      console.log(userObject)
+      setUser(userObject);
+      document.getElementById("signInDiv").hidden = true;
     }
+    
+    function handleSignOut(event) {
+      setUser({});
+      document.getElementById("signInDiv").hidden = false;
+    }
+  
+    //testing user authentication
+    useEffect(() => {
+      /* global google */
+      google.accounts.id.initialize({
+        client_id: "186661128169-rtbrhibr9p5ne088h88sssvl4sei2nto.apps.googleusercontent.com",
+        callback: handleCallbackResponse
+      });
+  
+      google.accounts.id.renderButton(
+        document.getElementById("signInDiv"),
+        { theme: "outline", sign: "large"}
+      );
+  
+      google.accounts.id.prompt();
+    }, []);
 
 
     return (
         <>
-        <div className="container">
+        <div id = "signInDiv"></div>
+        {  Object.keys(user).length != 0 &&
+          <button onClick={ (e) => handleSignOut(e)}>Sign Out</button>
+        }
+        { user && <div> 
+            <img src={user.picture}></img>
+            <p>{user.name}</p>
+        </div>
+        }
+        {/* <div className="container">
             <div className="row">
             <div className="col-md-6 offset-md-3">
                 <div className="card">
@@ -59,7 +99,7 @@ export default function Login() {
                 </div>
             </div>
             </div>
-        </div>
+        </div> */}
         </>
     );
     }
