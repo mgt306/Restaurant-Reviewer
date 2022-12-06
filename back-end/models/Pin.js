67a0d34cb038ae970da716ed624e180e89559f86
Anh Tran <101434320+Anhtrr@users.noreply.github.com>
2022-12-06 14:53:03 -0500

    update nested pinSchema so we can link restaurant with a singular review

--- back-end/models/Pin.js
+++ back-end/models/Pin.js
@@ -1,5 +1,50 @@
 const mongoose = require("mongoose");
 
+const ReviewSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      min: 1,
+      max: 60,
+    },
+    description: {
+      type: String,
+      required: false,
+      min: 1,
+      max: 200,
+    },
+    ambianceRating: {
+      type: String,
+      required: true,
+    },
+    foodRating: {
+      type: String,
+      required: true,
+    },
+    serviceRating: {
+      type: String,
+      required: true,
+    },
+    priceRating: {
+      type: String,
+      required: true,
+    },
+    overallRating: {
+      type: String,
+      required: true,
+    },
+    userId: {
+      type: String,
+      required: false,
+    },
+    restaurantId: {
+      type: String,
+      required: false,
+    }
+  },
+  { timestamps: true }
+);
 const PinSchema = new mongoose.Schema(
     {
       name: {
@@ -24,8 +69,11 @@ const PinSchema = new mongoose.Schema(
         type: String,
         required: true,
       },
+      review: {
+        type: ReviewSchema,
+        required: false,
+      }
     },
     { timestamps: true }
-  );
-  
+);  
   module.exports = mongoose.model("Pin", PinSchema);
