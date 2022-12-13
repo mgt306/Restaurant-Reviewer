b2e4623bc0a54cbae2e8e4248e3d5d239e795690
Anh Tran <101434320+Anhtrr@users.noreply.github.com>
2022-12-13 13:56:06 -0500

    Update viewResto.js

--- front-end/src/Pages/ViewResto/viewResto.js
+++ front-end/src/Pages/ViewResto/viewResto.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { useState, useEffect } from "react";
 import axios from "axios";
+import "./ViewResto.css";
 
 const ViewResto = () => {
     
@@ -22,7 +23,7 @@ const ViewResto = () => {
     }, []);
 
     return (  
-        <div className=''>
+        <div className='restoo'>
             {pin.length !== 0 &&(
                 <card className='restaurantProfile'>
                     <label className='restaurantlabels'>RESTAURANT NAME</label>
@@ -30,7 +31,6 @@ const ViewResto = () => {
                     <label className='restaurantlabels'>RESTAURANT INFO</label>
                     <h5 className='restaurantinfo'> Address: {pin.address}</h5>
                     <h5 className='restaurantinfo'> Phone: {pin.phone}</h5>
-                    <h5 className='restaurantinfo'> Cuisine Categories: {pin.cuisine}</h5>
                     <label className='restaurantlabels'>BOOKING INFO</label>
                     <p></p>
                     {pin.booking.length !== 0 &&(
@@ -47,7 +47,7 @@ const ViewResto = () => {
                     )}
                     <label className='restaurantlabels'>RESTAURANT IMAGE</label>
                     <p></p>
-                    <img src={pin.images} className='restaurantImage'></img>
+                    <img className='restaurantImage' src={pin.images} ></img>
                 </card>
             )}
         </div>
