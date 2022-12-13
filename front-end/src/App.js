e0d4e1b69c8428da49e4d267c9976123fc97891b
Anh Tran <101434320+Anhtrr@users.noreply.github.com>
2022-12-13 13:35:31 -0500

    Update App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,44 +1,13 @@
-import './App.css';
 import Navbar from './components/Navbar';
 import AboutUs from './Pages/About/AboutUs';
 import Home from './Pages/Home/Home';
-import Profile from './Pages/Profile/Profile';
 import Explore from './Pages/Explore/Explore';
 import { Route, Routes, Navigate } from 'react-router-dom';
-import Login from './Pages/Login and Register/Login';
-import Register from './Pages/Login and Register/Register';
 import GenericReview from './Pages/Register/GenericReview';
-import { useEffect, useState } from 'react';
-import jwt_decode from "jwt-decode";
+import ViewResto from './Pages/ViewResto/viewResto';
 
-
-function App() {
-  const [user, setUser] = useState(null);
-  useEffect(() => {
-    const getUser = () => {
-      fetch("http://localhost:3000/auth/login/success", {
-        method: "GET",
-        credentials: "include",
-        headers: {
-          Accept: "application/json",
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-        },
-      })
-        .then((response) => {
-          if (response.status === 200) return response.json();
-          throw new Error("authentication has been failed!");
-        })
-        .then((resObject) => {
-          setUser(resObject.user);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-    };
-    getUser();
-  }, []);
-
+function App() {  
+  
   
   return (
     <div>
@@ -47,13 +16,10 @@ function App() {
           <Routes>
             <Route path ='/' element={<Home/>}/>
             <Route path ='/about' element={<AboutUs/>}/>
-            <Route path ='/profile' element={<Profile/>}/>
             <Route path ='/explore' element={<Explore/>}/>
             <Route path ='*' element={ <Navigate to ='/'/> } />
-            <Route path="/login" element={user ? <Navigate to="/" /> : <Login />} />
-            <Route path = '/login' element={<Login/>}/>
-            <Route path = '/register' element={<Register/>}/>
-            <Route path = '/review' element={<GenericReview/>}/>
+            <Route path = '/review/:RestaurantId' element={<GenericReview/>}/>
+            <Route path = '/viewResto/:RestaurantId' element={<ViewResto/>}/>
           </Routes>
         </main>
     </div>
