54aceca007f91007f16f7f43246ceb1d304b2184
mgt306 <98681600+mgt306@users.noreply.github.com>
2022-12-14 15:11:37 -0500

    Update GenericReview.js

--- front-end/src/Pages/Register/GenericReview.js
+++ front-end/src/Pages/Register/GenericReview.js
@@ -1,4 +1,4 @@
-import { useParams, Navigate } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import "./GenericReview.css";
 import { useState, useEffect } from "react";
 import { React } from "react";
